540fb85be2eea31e1df8dc4fed2d6309f5e6d807
ch <mkmsacc@outlook.com>
2022-09-03 00:26:23 +0700

    Add integration test

---
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+        properties = {"my.property=hello"})
+class DemoApplicationTests {
+
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Test
+    void shouldReturnHello() {
+        webTestClient.get()
+                .uri("/")
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful()
+                .expectBody(String.class)
+                .isEqualTo("hello");
+
+    }
+}
