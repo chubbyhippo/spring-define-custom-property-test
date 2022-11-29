9be6458abe10bd950956decb38e971a877454f1b
ch <mkmsacc@outlook.com>
2022-11-29 13:18:59 +0700

    Add / to path mapping

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @GetMapping
+    @GetMapping("/")
     public String getHello(@Value("${my.property}") String hello) {
         return hello;
     }
